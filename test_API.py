803cc59ec43cabdeb1402abc406763027b3fa272
r.panshin <rpanshin@gmail.com>
2022-02-14 01:28:47 +0500

    Initial commit

---
+++ test_API.py
@@ -0,0 +1,15 @@
+import requests
+username = 'RomanPanshin'
+token = '4176a2a269b81c9c3325873e17db3c788a8400a7'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
